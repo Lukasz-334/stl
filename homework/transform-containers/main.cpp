6bcfdde6825e7f4910f9ba359ae5af5233e78176
Łukasz Nitka <lukasnit@wp.pl>
2021-08-09 09:15:10 +0200

    size_validation_test

--- homework/transform-containers/main.cpp
+++ homework/transform-containers/main.cpp
@@ -3,6 +3,10 @@
 #include "transform_containers.hpp"
 
 int main() {
-    
+    std::list<std::string> test = {"abc", "ff", "aaa", "abc", "ba", "cc", "ba", "a"};
+    std::deque<int> test2 = {2, 4, 5, 1, 1, 2, 6, 6, 6, 6, 6, 3, 3, 3,12};
+
+    removeDuplicateAndTranformToMap(test, test2);
+
     return 0;
 }
--- homework/transform-containers/transform_containers.cpp
+++ homework/transform-containers/transform_containers.cpp
@@ -6,6 +6,7 @@ std::pair<int, std::string> func(int x, std::string y) {
 
 std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string> list_s, std::deque<int> deque_int) {
     std::map<int, std::string> map_int_string;
+   
     list_s.sort();
     auto last = std::unique(list_s.begin(), list_s.end());
     list_s.erase(last, list_s.end());
@@ -13,8 +14,12 @@ std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string
     sort(deque_int.begin(), deque_int.end());
     auto last2 = std::unique(deque_int.begin(), deque_int.end());
     deque_int.erase(last2, deque_int.end());
-
-    std::transform(begin(deque_int), end(deque_int), begin(list_s), std::inserter(map_int_string, map_int_string.end()), func);
-
+     
+    if(list_s.size()==deque_int.size()){
+    std::transform(begin(deque_int), end(deque_int), begin(list_s), std::inserter(map_int_string, map_int_string.end()), func);  
+    }
+    else{
+        std::cout<<"wrong container size "<<std::endl;
+    }
     return map_int_string;
 }
